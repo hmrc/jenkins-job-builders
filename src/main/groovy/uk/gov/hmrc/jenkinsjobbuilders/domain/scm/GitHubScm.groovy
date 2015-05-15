8b298d6996d9dbca905b2e72eff787b45e31e305
Steve Smith <steve@alwaysagileconsulting.com>
2015-05-15 11:59:51 +0100

    MDTP-32 Add optional support for github refspec and credentials

--- src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/scm/GitHubScm.groovy
+++ src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/scm/GitHubScm.groovy
@@ -5,16 +5,24 @@ final class GitHubScm implements Scm {
     private final String protocol
     private final String host
     private final String branch
+    private final String refspec
+    private final String credentials
 
-    private GitHubScm(String host, String repository, String branch, String protocol) {
+    private GitHubScm(String host, String repository, String branch, String protocol, String refspec, String credentials) {
         this.branch = branch
         this.host = host
         this.protocol = protocol
         this.repository = repository
+        this.refspec = refspec
+        this.credentials = credentials
     }
 
     static GitHubScm gitHubScm(String host, String repository, String branch, String protocol) {
-        new GitHubScm(host, repository, branch, protocol)
+        gitHubScm(host, repository, branch, protocol, null, null)
+    }
+
+    static GitHubScm gitHubScm(String host, String repository, String branch, String protocol, String refspec, String credentials) {
+        new GitHubScm(host, repository, branch, protocol, refspec, credentials)
     }
 
     @Override
@@ -24,6 +32,13 @@ final class GitHubScm implements Scm {
                 remote {
                     github(repository, protocol, host)
                 }
+                if (this.refspec != null) {
+                    refspec(this.refspec)
+                }
+                if (this.credentials != null) {
+                    credentials(this.credentials)
+                }
+
                 branch(this.branch)
             }
         }
