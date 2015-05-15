72adfa9e5c399becc4dc56250e4bec3a47b4798c
Steve Smith <steve@alwaysagileconsulting.com>
2015-05-15 12:07:06 +0100

    MDTP-32 Add optional support for github refspec and credentials

--- src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/scm/GitHubComScm.groovy
+++ src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/scm/GitHubComScm.groovy
@@ -3,12 +3,16 @@ package uk.gov.hmrc.jenkinsjobbuilders.domain.scm
 final class GitHubComScm implements Scm {
     private final Scm scm
 
-    private GitHubComScm(String repository) {
-        this.scm = GitHubScm.gitHubScm('github.com', repository, 'master', 'ssh')
+    private GitHubComScm(String repository, String credentials) {
+        this.scm = GitHubScm.gitHubScm('github.com', repository, 'master', 'ssh', null, credentials)
     }
 
     static GitHubComScm gitHubComScm(String repository) {
-        new GitHubComScm(repository)
+        gitHubComScm(repository, null)
+    }
+
+    static GitHubComScm gitHubComScm(String repository, String credentials) {
+        new GitHubComScm(repository, credentials)
     }
 
     @Override
