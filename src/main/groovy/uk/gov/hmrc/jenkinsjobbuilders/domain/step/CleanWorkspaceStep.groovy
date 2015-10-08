e26d5444b4f11bc46e5b4b2c399f5ba9c762f15e
Steve Smith <steve@alwaysagileconsulting.com>
2015-10-08 15:10:30 +0100

    MDTP-164 Clean workspace step

---
+++ src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/step/CleanWorkspaceStep.groovy
@@ -0,0 +1,22 @@
+package uk.gov.hmrc.jenkinsjobbuilders.domain.step
+
+class CleanWorkspaceStep implements Step {
+    private final Step step
+
+    private CleanWorkspaceStep() {
+        this.step = shellStep("""
+                              |cd \${WORKSPACE}
+                              |find target -maxdepth 1 -type d -not -name '*reports' -not -name 'target' | xargs rm -rf
+                              |find . -maxdepth 1 -type d -not -name 'target' -not -name '.*' -not -name 'logs' | xargs rm -rf
+                              """.stripMargin())
+    }
+
+    public static Step cleanWorkspace() {
+        new CleanWorkspaceStep()
+    }
+
+    @Override
+    Closure toDsl() {
+        return step.toDsl()
+    }
+}
