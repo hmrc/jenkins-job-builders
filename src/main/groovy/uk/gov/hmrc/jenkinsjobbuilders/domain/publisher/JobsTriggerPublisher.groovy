181d0e3190ffcf8903c7db3a0b82fb09759badbc
Steve Smith <steve@alwaysagileconsulting.com>
2016-01-19 15:48:02 +0000

    Optional condition for non-blocking jobs trigger publisher

--- src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/publisher/JobsTriggerPublisher.groovy
+++ src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/publisher/JobsTriggerPublisher.groovy
@@ -4,23 +4,25 @@ import static java.util.Collections.emptyMap
 
 
 final class JobsTriggerPublisher implements Publisher {
-    private final String name
-    private final Map<String, String> parameters = new HashMap()
+    private final def name
+    private final def condition
+    private final def parameters = new HashMap()
 
-    private JobsTriggerPublisher(String name, Map<String, String> parameters) {
+    private JobsTriggerPublisher(String name, String condition, Map<String, String> parameters) {
         this.name = name
+        this.condition = condition
         this.parameters.putAll(parameters)
     }
 
-    static JobsTriggerPublisher jobsTriggerPublisher(String name, Map<String, String> parameters = emptyMap()) {
-        new JobsTriggerPublisher(name, parameters)
+    static JobsTriggerPublisher jobsTriggerPublisher(String name, String condition = 'SUCCESS', Map<String, String> parameters = emptyMap()) {
+        new JobsTriggerPublisher(name, condition, parameters)
     }
 
     @Override
     Closure toDsl() {
         return {
             downstreamParameterized {
-                trigger(name, 'SUCCESS', parameters.isEmpty()) {
+                trigger(name, condition, parameters.isEmpty()) {
                     predefinedProps(parameters)
                 }
             }
