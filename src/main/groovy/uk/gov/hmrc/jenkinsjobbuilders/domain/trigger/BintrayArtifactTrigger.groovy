47ed61a48c3ec8c08589db0c1ab6dce6ea72d787
Andrew Gee <andrew.w.gee@accenture.com>
2016-02-09 16:18:07 +0000

    Added a generic Bintray watcher trigger

---
+++ src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/trigger/BintrayArtifactTrigger.groovy
@@ -0,0 +1,36 @@
+package uk.gov.hmrc.jenkinsjobbuilders.domain.trigger;
+
+public class BintrayArtifactTrigger implements Trigger {
+    private final String cronSchedule
+    private final String subject
+    private final String repo
+    private final List<String> packagesToPoll
+
+    private BintrayArtifactTrigger(String cronSchedule, String subject, String repo, List<String> packagesToPoll) {
+        this.cronSchedule = cronSchedule
+        this.subject = subject
+        this.repo = repo
+        this.packagesToPoll = packagesToPoll
+    }
+
+    static BintrayArtifactTrigger bintrayArtifactTrigger(String cron, String subject, String repo, List<String> packagesToPoll) {
+        new BintrayArtifactTrigger(cron, subject, repo, packagesToPoll)
+    }
+
+    @Override
+    Closure toDsl() {
+        return {
+            urlTrigger{
+                cron(cronSchedule)
+
+                packagesToPoll.each{
+                    url(String.format("https://api.bintray.com/packages/%s/%s/%s/", subject, repo, it)){
+                        inspection('json'){
+                            path('latest_version')
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
