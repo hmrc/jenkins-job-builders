4e78e1845546764f68ac4ec5cdffb0269b8c53c9
Adam Hansrod <adam.hansrod@digital.hmrc.gov.uk>
2017-05-16 14:02:42 +0100

    PBD-18: Use existing enums for permissions over strings

--- src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/authorisation/Permission.groovy
+++ src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/authorisation/Permission.groovy
@@ -1,25 +1,26 @@
 package uk.gov.hmrc.jenkinsjobbuilders.domain.authorisation
 
+import javaposse.jobdsl.dsl.helpers.Permissions
 import uk.gov.hmrc.jenkinsjobbuilders.domain.Setting
 
 class Permission implements Setting {
 
-    final String permissionIdentifier
+    final Permissions permissionEnum
     final String ldapIdentifier
 
-    private Permission(String ldapIdentifier, String permissionIdentifier) {
+    private Permission(String ldapIdentifier, Permissions permissionEnum) {
         this.ldapIdentifier = ldapIdentifier
-        this.permissionIdentifier = permissionIdentifier
+        this.permissionEnum = permissionEnum
     }
 
-    static Permission permissionSetting(String ldapIdentifier, String permission) {
+    static Permission permissionSetting(String ldapIdentifier, Permissions permission) {
         new Permission(ldapIdentifier, permission)
     }
 
     @Override
     Closure toDsl() {
         return {
-            permission(permissionIdentifier, ldapIdentifier)
+            permission(permissionEnum, ldapIdentifier)
         }
     }
 }
