bf782c32d4dad6d4ca792ac9c04b1bc0bbd5d4b6
Andrew Heald <andrew.heald@digital.hmrc.gov.uk>
2017-08-18 14:47:51 +0100

    Revert "PBD-196: Re-implement JobsTriggerStep as a raw XML configuration."
    This reverts commit 82e1b0222b0d42461fa02fc24605fabfd6ce15bc.

--- src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/step/JobsTriggerStep.groovy
+++ src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/step/JobsTriggerStep.groovy
@@ -1,20 +1,25 @@
-package uk.gov.hmrc.jenkinsjobbuilders.domain.configure
+package uk.gov.hmrc.jenkinsjobbuilders.domain.step
 
-final class JobsTriggerStep implements Configure {
-    private final List<String> projectsToBuild
+import static java.util.Collections.emptyMap
+
+final class JobsTriggerStep implements Step {
+    private final def name
+    private final def parameters = new HashMap()
     private final String filePattern
     private final String noFilesFoundAction
     private final String buildStepFailureThreshold
     private final String failureThreshold
     private final String unstableThreshold
 
-    private JobsTriggerStep(List<String> projectsToBuild,
+    private JobsTriggerStep(String name,
+                            Map<String, String> parameters,
                             String filePattern,
                             String noFilesFoundAction,
                             String buildStepFailureThreshold,
                             String failureThreshold,
                             String unstableThreshold) {
-        this.projectsToBuild = projectsToBuild
+        this.name = name
+        this.parameters.putAll(parameters)
         this.filePattern = filePattern
         this.noFilesFoundAction = noFilesFoundAction
         this.buildStepFailureThreshold = buildStepFailureThreshold
@@ -22,35 +27,38 @@ final class JobsTriggerStep implements Configure {
         this.unstableThreshold = unstableThreshold
     }
 
-    static Configure jobsTriggerStep(List<String> projectsToBuild,
+    static Step jobsTriggerStep(String name,
+                                Map<String, String> parameters = emptyMap(),
                                 String filePattern,
                                 String noFilesFoundAction = 'SKIP',
                                 String buildStepFailureThreshold = 'FAILURE',
                                 String failureThreshold = 'FAILURE',
                                 String unstableThreshold = 'UNSTABLE') {
-        new JobsTriggerStep(projectsToBuild, filePattern, noFilesFoundAction,
+        new JobsTriggerStep(name, parameters, filePattern, noFilesFoundAction,
                 buildStepFailureThreshold, failureThreshold, unstableThreshold)
     }
 
     @Override
     Closure toDsl() {
         return {
-            def triggerConfig = it / builders /
-                    'hudson.plugins.parameterizedtrigger.TriggerBuilder' /
-                    configs /
-                    'hudson.plugins.parameterizedtrigger.BlockableBuildTriggerConfig'
-            triggerConfig / projects(this.projectsToBuild.join(','))
-            if (this.filePattern) {
-                triggerConfig / configFactories /
-                        'hudson.plugins.parameterizedtrigger.FileBuildParameterFactory' {
-                            filePattern(this.filePattern)
-                            noFilesFoundAction(this.noFilesFoundAction)
+            downstreamParameterized {
+                trigger(name) {
+                    if (this.parameters) {
+                        parameters {
+                            predefinedProps(this.parameters)
                         }
-            }
-            triggerConfig / block {
-                buildStepFailureThreshold { name(this.buildStepFailureThreshold) }
-                failureThreshold { name(this.failureThreshold) }
-                unstableThreshold { name(this.unstableThreshold) }
+                    }
+                    block {
+                        buildStepFailure(buildStepFailureThreshold)
+                        failure(failureThreshold)
+                        unstable(unstableThreshold)
+                    }
+                    if (filePattern) {
+                        parameterFactories {
+                            forMatchingFiles(filePattern, noFilesFoundAction)
+                        }
+                    }
+                }
             }
         }
     }
---
+++ src/test/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/step/JobsTriggerStepSpec.groovy
@@ -0,0 +1,72 @@
+package uk.gov.hmrc.jenkinsjobbuilders.domain.step
+
+import javaposse.jobdsl.dsl.Job
+import spock.lang.Specification
+import uk.gov.hmrc.jenkinsjobbuilders.domain.JobParents
+import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.JobBuilder
+
+import static uk.gov.hmrc.jenkinsjobbuilders.domain.step.JobsTriggerStep.jobsTriggerStep
+
+@Mixin(JobParents)
+class JobsTriggerStepSpec extends Specification {
+
+    void 'test XML output with predefined build parameters'() {
+        given:
+        Step step = jobsTriggerStep('test-project',
+                                    ['key1': 'value1', 'key2': 'value2'],
+                                    '*.properties',
+                                    'FAIL',
+                                    'SUCCESS',
+                                    'UNSTABLE',
+                                    'FAILURE')
+        JobBuilder jobBuilder = new JobBuilder('test-job', 'test-job-description').
+                                               withSteps(step)
+
+        when:
+        Job job = jobBuilder.build(jobParent())
+
+        then:
+        with(job.node) {
+            def buildTriggerConfig = builders
+                    .'hudson.plugins.parameterizedtrigger.TriggerBuilder'.configs
+                    .'hudson.plugins.parameterizedtrigger.BlockableBuildTriggerConfig'
+            buildTriggerConfig.projects.text() == 'test-project'
+            buildTriggerConfig.condition.text() == 'ALWAYS'
+            buildTriggerConfig.configs.'hudson.plugins.parameterizedtrigger.PredefinedBuildParameters'
+                    .properties.text() == "key1=value1\nkey2=value2"
+            buildTriggerConfig.block.buildStepFailureThreshold.name.text() == 'SUCCESS'
+            buildTriggerConfig.block.failureThreshold.name.text() == 'UNSTABLE'
+            buildTriggerConfig.block.unstableThreshold.name.text() == 'FAILURE'
+        }
+    }
+
+    void 'test XML output without predefined build parameters'() {
+        given:
+        Step step = jobsTriggerStep('test-project',
+                                    [:],
+                                    '*.properties',
+                                    'FAIL',
+                                    'SUCCESS',
+                                    'UNSTABLE',
+                                    'FAILURE')
+        JobBuilder jobBuilder = new JobBuilder('test-job', 'test-job-description').
+                withSteps(step)
+
+        when:
+        Job job = jobBuilder.build(jobParent())
+
+        then:
+        with(job.node) {
+            def buildTriggerConfig = builders
+                    .'hudson.plugins.parameterizedtrigger.TriggerBuilder'.configs
+                    .'hudson.plugins.parameterizedtrigger.BlockableBuildTriggerConfig'
+            buildTriggerConfig.projects.text() == 'test-project'
+            buildTriggerConfig.condition.text() == 'ALWAYS'
+            buildTriggerConfig.configs.'hudson.plugins.parameterizedtrigger.PredefinedBuildParameters'
+                    .properties.text() == ""
+            buildTriggerConfig.block.buildStepFailureThreshold.name.text() == 'SUCCESS'
+            buildTriggerConfig.block.failureThreshold.name.text() == 'UNSTABLE'
+            buildTriggerConfig.block.unstableThreshold.name.text() == 'FAILURE'
+        }
+    }
+}
