87c434a22a7701292120e66e066bdb77f8158ebb
Edd Grant <edd.grant@digital.hmrc.gov.uk>
2017-08-18 17:28:53 +0100

    PLATOPS-196 - Implement the missing 'threshold' XML elements.

--- src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/configure/JobsTriggerStep.groovy
+++ src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/configure/JobsTriggerStep.groovy
@@ -1,19 +1,49 @@
 package uk.gov.hmrc.jenkinsjobbuilders.domain.configure
 
+import static uk.gov.hmrc.jenkinsjobbuilders.domain.configure.JobsTriggerStep.Threshold.FAILURE
+import static uk.gov.hmrc.jenkinsjobbuilders.domain.configure.JobsTriggerStep.Threshold.NEVER
+
 final class JobsTriggerStep implements Configure {
+
+    enum Threshold {
+        /**
+         * We don't actually use 'NEVER' but it
+         * seemed confusing not to provide it as an option.
+         */
+        NEVER('', -1, '', true),
+        SUCCESS('SUCCESS', 0, 'BLUE', true),
+        UNSTABLE('UNSTABLE', 1, 'YELLOW', true),
+        FAILURE('FAILURE', 2, 'RED', true)
+
+        Threshold(String name,
+                  int ordinal,
+                  String color,
+                  boolean completeBuild) {
+            this.name = name
+            this.ordinal = ordinal
+            this.color = color
+            this.completeBuild = completeBuild
+        }
+
+        private final String name
+        private final int ordinal
+        private final String color
+        private final String completeBuild
+    }
+
     private final List<String> projectsToBuild
     private final String filePattern
     private final String noFilesFoundAction
-    private final String buildStepFailureThreshold
-    private final String failureThreshold
-    private final String unstableThreshold
+    private final Threshold buildStepFailureThreshold
+    private final Threshold failureThreshold
+    private final Threshold unstableThreshold
 
     private JobsTriggerStep(List<String> projectsToBuild,
                             String filePattern,
                             String noFilesFoundAction,
-                            String buildStepFailureThreshold,
-                            String failureThreshold,
-                            String unstableThreshold) {
+                            Threshold buildStepFailureThreshold,
+                            Threshold failureThreshold,
+                            Threshold unstableThreshold) {
         this.projectsToBuild = projectsToBuild
         this.filePattern = filePattern
         this.noFilesFoundAction = noFilesFoundAction
@@ -23,11 +53,11 @@ final class JobsTriggerStep implements Configure {
     }
 
     static Configure jobsTriggerStep(List<String> projectsToBuild,
-                                String filePattern,
-                                String noFilesFoundAction = 'SKIP',
-                                String buildStepFailureThreshold = 'FAILURE',
-                                String failureThreshold = 'FAILURE',
-                                String unstableThreshold = 'UNSTABLE') {
+                                     String filePattern,
+                                     String noFilesFoundAction = 'SKIP',
+                                     Threshold buildStepFailureThreshold = FAILURE,
+                                     Threshold failureThreshold = FAILURE,
+                                     Threshold unstableThreshold = FAILURE) {
         new JobsTriggerStep(projectsToBuild, filePattern, noFilesFoundAction,
                 buildStepFailureThreshold, failureThreshold, unstableThreshold)
     }
@@ -48,9 +78,30 @@ final class JobsTriggerStep implements Configure {
                         }
             }
             triggerConfig / block {
-                buildStepFailureThreshold { name(this.buildStepFailureThreshold) }
-                failureThreshold { name(this.failureThreshold) }
-                unstableThreshold { name(this.unstableThreshold) }
+                if(this?.buildStepFailureThreshold != NEVER) {
+                    delegate.buildStepFailureThreshold {
+                        name(this.buildStepFailureThreshold.name)
+                        ordinal(this.buildStepFailureThreshold.ordinal)
+                        color(this.buildStepFailureThreshold.color)
+                        completeBuild(this.buildStepFailureThreshold.completeBuild)
+                    }
+                }
+                if(this?.failureThreshold != NEVER) {
+                    delegate.failureThreshold {
+                        name(this.failureThreshold.name)
+                        ordinal(this.failureThreshold.ordinal)
+                        color(this.failureThreshold.color)
+                        completeBuild(this.failureThreshold.completeBuild)
+                    }
+                }
+                if(this?.unstableThreshold != NEVER) {
+                    delegate.unstableThreshold {
+                        name(this.unstableThreshold.name)
+                        ordinal(this.unstableThreshold.ordinal)
+                        color(this.unstableThreshold.color)
+                        completeBuild(this.unstableThreshold.completeBuild)
+                    }
+                }
             }
         }
     }
--- src/test/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/configure/JobsTriggerStepSpec.groovy
+++ src/test/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/configure/JobsTriggerStepSpec.groovy
@@ -13,11 +13,11 @@ class JobsTriggerStepSpec extends Specification {
     void 'test XML output'() {
         given:
         Configure step = jobsTriggerStep(['test-project-1', 'test-project-2'],
-                                    '*.properties',
-                                    'FAIL',
-                                    'SUCCESS',
-                                    'UNSTABLE',
-                                    'FAILURE')
+                                         '*.properties',
+                                         'FAIL',
+                                         JobsTriggerStep.Threshold.SUCCESS,
+                                         JobsTriggerStep.Threshold.UNSTABLE,
+                                         JobsTriggerStep.Threshold.FAILURE)
         JobBuilder jobBuilder = new JobBuilder('test-job', 'test-job-description').
                                                withConfigures(step)
 
@@ -33,9 +33,58 @@ class JobsTriggerStepSpec extends Specification {
             def paramFactory = triggerConfig.configFactories.'hudson.plugins.parameterizedtrigger.FileBuildParameterFactory'
             paramFactory.filePattern.text() == '*.properties'
             paramFactory.noFilesFoundAction.text() == 'FAIL'
+
+            assert 3 == triggerConfig.block[0].children().size()
+
             triggerConfig.block.buildStepFailureThreshold.name.text() == 'SUCCESS'
+            triggerConfig.block.buildStepFailureThreshold.ordinal.text() == '0'
+            triggerConfig.block.buildStepFailureThreshold.color.text() == 'BLUE'
+            triggerConfig.block.buildStepFailureThreshold.completeBuild.text() == 'true'
+
             triggerConfig.block.failureThreshold.name.text() == 'UNSTABLE'
+            triggerConfig.block.failureThreshold.ordinal.text() == '1'
+            triggerConfig.block.failureThreshold.color.text() == 'YELLOW'
+            triggerConfig.block.failureThreshold.completeBuild.text() == 'true'
+
             triggerConfig.block.unstableThreshold.name.text() == 'FAILURE'
+            triggerConfig.block.unstableThreshold.ordinal.text() == '2'
+            triggerConfig.block.unstableThreshold.color.text() == 'RED'
+            triggerConfig.block.unstableThreshold.completeBuild.text() == 'true'
         }
     }
+
+    void 'it should not write XML elements for thresholds of never'() {
+        given:
+        Configure step = jobsTriggerStep(['test-project-1', 'test-project-2'],
+                                         '*.properties',
+                                         'FAIL',
+                                         JobsTriggerStep.Threshold.SUCCESS,
+                                         JobsTriggerStep.Threshold.NEVER,
+                                         JobsTriggerStep.Threshold.FAILURE)
+        JobBuilder jobBuilder = new JobBuilder('test-job', 'test-job-description').
+                withConfigures(step)
+
+        when:
+        Job job = jobBuilder.build(jobParent())
+
+        then:
+        with(job.node) {
+            def triggerConfig = builders
+                    .'hudson.plugins.parameterizedtrigger.TriggerBuilder'.configs
+                    .'hudson.plugins.parameterizedtrigger.BlockableBuildTriggerConfig'
+
+            assert 2 == triggerConfig.block[0].children().size() // There is no 'failureThreshold' element
+
+            triggerConfig.block.buildStepFailureThreshold.name.text() == 'SUCCESS'
+            triggerConfig.block.buildStepFailureThreshold.ordinal.text() == '0'
+            triggerConfig.block.buildStepFailureThreshold.color.text() == 'BLUE'
+            triggerConfig.block.buildStepFailureThreshold.completeBuild.text() == 'true'
+
+            triggerConfig.block.unstableThreshold.name.text() == 'FAILURE'
+            triggerConfig.block.unstableThreshold.ordinal.text() == '2'
+            triggerConfig.block.unstableThreshold.color.text() == 'RED'
+            triggerConfig.block.unstableThreshold.completeBuild.text() == 'true'
+        }
+    }
+
 }
