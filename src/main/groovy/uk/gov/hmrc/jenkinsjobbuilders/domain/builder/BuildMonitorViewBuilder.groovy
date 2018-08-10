af26f9f27a53f02aa097fd941345a922bd2d5feb
Jakob Grunig <jakob.grunig@digital.hmrc.gov.uk>
2018-08-10 09:42:01 +0100

    PLATOPS-1518 - support for regex filter in view

--- src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/builder/BuildMonitorViewBuilder.groovy
+++ src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/builder/BuildMonitorViewBuilder.groovy
@@ -3,15 +3,24 @@ package uk.gov.hmrc.jenkinsjobbuilders.domain.builder
 import javaposse.jobdsl.dsl.DslFactory
 import javaposse.jobdsl.dsl.View
 import javaposse.jobdsl.dsl.views.BuildMonitorView
+import javaposse.jobdsl.dsl.views.jobfilter.MatchType
+import javaposse.jobdsl.dsl.views.jobfilter.RegexMatchValue
 
 
 final class BuildMonitorViewBuilder implements Builder<View> {
 
     private final String name
     private String [] jobs
+    private String includeRegex
 
-    BuildMonitorViewBuilder(String name) {
-        this.name = name
+    BuildMonitorViewBuilder(String nameOrTeam, String name = '') {
+
+        // nameOrTeam is there for backwards compatibility, where the name was the first parameter
+        if(name == '') {
+            this.name = name
+        } else {
+            this.name = "$nameOrTeam/$name"
+        }
     }
 
     BuildMonitorViewBuilder withJobs(String ... jobs) {
@@ -19,11 +28,20 @@ final class BuildMonitorViewBuilder implements Builder<View> {
         this
     }
 
+    BuildMonitorViewBuilder withJobsMatching(String includeRegex) {
+        this.includeRegex = includeRegex
+        this
+    }
+
+
     @Override
     View build(DslFactory dslFactory) {
         dslFactory.buildMonitorView(this.name) {
             jobs {
                 names(this.jobs)
+                if(includeRegex != null) {
+                    regex(includeRegex)
+                }
             }
         }
     }
