b4017c92a9ed8773217f57f52dc501ec8574de0f
Dario Abdulrehman <dario.rehman@gmail.com>
2018-10-19 10:03:55 +0100

    PLATOPS-1585: Add upstream trigger
    Based on ReverseBuildTrigger, this trigger enables building a job based
    on the successful completion of upstream jobs.

---
+++ src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/Condition.groovy
@@ -0,0 +1,5 @@
+package uk.gov.hmrc.jenkinsjobbuilders.domain
+
+enum Condition {
+    SUCCESS, UNSTABLE, FAILURE
+}
---
+++ src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/trigger/UpstreamTrigger.groovy
@@ -0,0 +1,30 @@
+package uk.gov.hmrc.jenkinsjobbuilders.domain.trigger
+
+import uk.gov.hmrc.jenkinsjobbuilders.domain.Condition
+
+class UpstreamTrigger implements Trigger {
+    String upstreamJobs
+    Condition condition
+
+    private UpstreamTrigger(String upstreamJobs, Condition condition) {
+        this.upstreamJobs = upstreamJobs
+        this.condition = condition
+    }
+
+    /**
+     * Creates a {@link jenkins.triggers.ReverseBuildTrigger} based on the successful completion of any of a list of upstream jobs.
+     * @param upstreamJobs String with a comma separated list of jobs
+     * @param {@link uk.gov.hmrc.jenkinsjobbuilders.domain.Condition} one of SUCCESS, UNSTABLE, FAILURE
+     * @return UpstreamTrigger
+     */
+    static UpstreamTrigger upstreamTrigger(String upstreamJobs, Condition condition) {
+        new UpstreamTrigger(upstreamJobs, condition)
+    }
+
+    @Override
+    Closure toDsl() {
+        return {
+            upstream(upstreamJobs, condition.name())
+        }
+    }
+}
