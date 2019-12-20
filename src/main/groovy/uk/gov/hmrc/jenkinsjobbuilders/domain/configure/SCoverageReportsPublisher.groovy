a09a641e1ae619eeb6638c80370e394cf4cfc7c2
Artur Opala <250927+arturopala@users.noreply.github.com>
2019-12-20 11:37:31 +0000

    support different scala versions in scoverage report publisher

--- src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/configure/SCoverageReportsPublisher.groovy
+++ src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/configure/SCoverageReportsPublisher.groovy
@@ -2,18 +2,26 @@ package uk.gov.hmrc.jenkinsjobbuilders.domain.configure
 
 final class SCoverageReportsPublisher implements Configure {
 
-    private SCoverageReportsPublisher() {}
+    private final String scalaVersion
+
+    private SCoverageReportsPublisher(final String scalaVersion) {
+        this.scalaVersion = scalaVersion
+    }
 
     Closure toDsl() {
         return {
             it / 'publishers' / 'org.jenkinsci.plugins.scoverage.ScoveragePublisher' {
-                'reportDir'('target/scala-2.11/scoverage-report')
+                'reportDir'("target/scala-${scalaVersion}/scoverage-report")
                 'reportFile'('scoverage.xml')
             }
         }
     }
 
     static SCoverageReportsPublisher sCoverageReportsPublisher() {
-        new SCoverageReportsPublisher()
+        new SCoverageReportsPublisher("2.11")
+    }
+
+    static SCoverageReportsPublisher sCoverageReportsPublisher(final String scalaVersion) {
+        new SCoverageReportsPublisher(scalaVersion)
     }
 }
