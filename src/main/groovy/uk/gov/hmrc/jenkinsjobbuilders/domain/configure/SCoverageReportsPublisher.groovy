f9703364053f80580614e9f977f9550b470de97e
Pilar Gomez <678471+mpgomez@users.noreply.github.com>
2020-01-20 15:47:40 +0000

    PBD-2079: validate Scala version
    Add validation for the Scala version in SCoverageReportsPublisher

--- src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/configure/SCoverageReportsPublisher.groovy
+++ src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/configure/SCoverageReportsPublisher.groovy
@@ -1,11 +1,20 @@
 package uk.gov.hmrc.jenkinsjobbuilders.domain.configure
 
+import javaposse.jobdsl.dsl.DslScriptException
+
 final class SCoverageReportsPublisher implements Configure {
 
     private final String scalaVersion
+    static private final List<String> validScalaVersions = ["2.10", "2.11", "2.12"]
 
     private SCoverageReportsPublisher(final String scalaVersion) {
         this.scalaVersion = scalaVersion
+    }
+
+    static void validateScalaVersion(final String scalaVersion) {
+        if (! this.validScalaVersions.contains(scalaVersion) ) {
+            throw new DslScriptException("Could not use the Scala version ${scalaVersion}. The only supported Scala versions are ${this.validScalaVersions.join(",")}")
+        }
     }
 
     Closure toDsl() {
@@ -22,6 +31,7 @@ final class SCoverageReportsPublisher implements Configure {
     }
 
     static SCoverageReportsPublisher sCoverageReportsPublisher(final String scalaVersion) {
+        validateScalaVersion(scalaVersion)
         new SCoverageReportsPublisher(scalaVersion)
     }
 }
--- src/test/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/configure/SCoverageReportsPublisherSpec.groovy
+++ src/test/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/configure/SCoverageReportsPublisherSpec.groovy
@@ -1,5 +1,6 @@
 package uk.gov.hmrc.jenkinsjobbuilders.domain.configure
 
+import javaposse.jobdsl.dsl.DslScriptException
 import javaposse.jobdsl.dsl.Job
 import spock.lang.Specification
 import uk.gov.hmrc.jenkinsjobbuilders.domain.AbstractJobSpec
@@ -37,4 +38,17 @@ class SCoverageReportsPublisherSpec extends AbstractJobSpec {
             publishers.'org.jenkinsci.plugins.scoverage.ScoveragePublisher'.reportDir.text() == 'target/scala-2.12/scoverage-report'
         }
     }
+
+    void 'test invalid scala version' () {
+        given:
+        JobBuilder jobBuilder = new JobBuilder('test-job', 'test-job-description')
+
+        when:
+        jobBuilder.withConfigures(sCoverageReportsPublisher("2.09"))
+
+        then:
+        DslScriptException e = thrown()
+        e.message.contains("Could not use the Scala version 2.09. The only supported Scala versions are 2.10,2.11,2.12")
+    }
+
 }
