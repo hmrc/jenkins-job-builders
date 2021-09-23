52989bd513af6403883bee5560a5a554d093d2ff
Vlastimil Zeman <vlastimil@zeman.dev>
2021-09-23 15:12:16 +0100

    PSEC-1325 Refactor postBuildScriptPublisher
    - remove confusing boolean - string use for a build result
    - add ability to pass more than result (as when script should run)
    - use enum for build result to avoid typos

--- src/test/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/publisher/PostBuildScriptPublisherSpec.groovy
+++ src/test/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/publisher/PostBuildScriptPublisherSpec.groovy
@@ -3,15 +3,19 @@ package uk.gov.hmrc.jenkinsjobbuilders.domain.publisher
 import javaposse.jobdsl.dsl.Job
 import uk.gov.hmrc.jenkinsjobbuilders.domain.AbstractJobSpec
 import uk.gov.hmrc.jenkinsjobbuilders.domain.builder.JobBuilder
+import uk.gov.hmrc.jenkinsjobbuilders.domain.publisher.Result
 
 import static uk.gov.hmrc.jenkinsjobbuilders.domain.publisher.PostBuildScriptPublisher.postBuildScriptPublisher
 
 class PostBuildScriptPublisherSpec extends AbstractJobSpec {
 
-    void 'test XML output'() {
+    void 'test XML output with empty build stage result is still valid but it never runs'() {
         given:
-        JobBuilder jobBuilder = new JobBuilder('test-job', 'test-job-description').
-                                               withPublishers(postBuildScriptPublisher('test-script'))
+        JobBuilder jobBuilder = new JobBuilder('test-job', 'test-job-description')
+            .withPublishers(postBuildScriptPublisher(
+                'test-script',
+                []
+            ))
 
         when:
         Job job = jobBuilder.build(JOB_PARENT)
@@ -19,10 +23,53 @@ class PostBuildScriptPublisherSpec extends AbstractJobSpec {
         then:
         with(job.node) {
             def postBuildScript = publishers.'org.jenkinsci.plugins.postbuildscript.PostBuildScript'
-            postBuildScript.scriptOnlyIfSuccess.text() == 'false'
-            postBuildScript.scriptOnlyIfFailure.text() == 'true'
-            postBuildScript.markBuildUnstable.text() == 'false'
-            postBuildScript.buildSteps.'hudson.tasks.Shell'.command.text() == 'test-script'
+            postBuildScript.config.markBuildUnstable.text() == 'false'
+            def postBuildStep = postBuildScript.config.buildSteps.'org.jenkinsci.plugins.postbuildscript.model.PostBuildStep'
+            postBuildStep.buildSteps.'hudson.tasks.Shell'.command.text() == 'test-script'
+            postBuildStep.results.text() == ''
+        }
+    }
+
+    void 'test XML output with one build stage result'() {
+        given:
+        JobBuilder jobBuilder = new JobBuilder('test-job', 'test-job-description')
+            .withPublishers(postBuildScriptPublisher(
+                'test-script',
+                [Result.SUCCESS]
+            ))
+
+        when:
+        Job job = jobBuilder.build(JOB_PARENT)
+
+        then:
+        with(job.node) {
+            def postBuildScript = publishers.'org.jenkinsci.plugins.postbuildscript.PostBuildScript'
+            postBuildScript.config.markBuildUnstable.text() == 'false'
+            def postBuildStep = postBuildScript.config.buildSteps.'org.jenkinsci.plugins.postbuildscript.model.PostBuildStep'
+            postBuildStep.buildSteps.'hudson.tasks.Shell'.command.text() == 'test-script'
+            postBuildStep.results.string[0].text() == 'SUCCESS'
+        }
+    }
+
+    void 'test XML output with two build stage results'() {
+        given:
+        JobBuilder jobBuilder = new JobBuilder('test-job', 'test-job-description')
+            .withPublishers(postBuildScriptPublisher(
+                'test-script',
+                [Result.SUCCESS,Result.UNSTABLE]
+            ))
+
+        when:
+        Job job = jobBuilder.build(JOB_PARENT)
+
+        then:
+        with(job.node) {
+            def postBuildScript = publishers.'org.jenkinsci.plugins.postbuildscript.PostBuildScript'
+            postBuildScript.config.markBuildUnstable.text() == 'false'
+            def postBuildStep = postBuildScript.config.buildSteps.'org.jenkinsci.plugins.postbuildscript.model.PostBuildStep'
+            postBuildStep.buildSteps.'hudson.tasks.Shell'.command.text() == 'test-script'
+            postBuildStep.results.string[0].text() == 'SUCCESS'
+            postBuildStep.results.string[1].text() == 'UNSTABLE'
         }
     }
 }
