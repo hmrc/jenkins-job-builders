98cd11a61a2ae26da1f4edcbdc662e03964a472f
jeff-wong-hmrc <215989177+jeff-wong-hmrc@users.noreply.github.com>
2025-11-06 09:36:39 +0000

    PBD-5153 Adding regularExpressionForFailed and description ForFailed to BuildDescriptionPublisher

--- src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/AbstractBuildDescription.groovy
+++ src/main/groovy/uk/gov/hmrc/jenkinsjobbuilders/domain/AbstractBuildDescription.groovy
@@ -3,15 +3,19 @@ package uk.gov.hmrc.jenkinsjobbuilders.domain
 abstract class AbstractBuildDescription implements Setting {
     protected final String description
     protected final String regex
+    protected final String regularExpressionForFailed
+    protected final String descriptionForFailed
 
-    protected AbstractBuildDescription(String regex, String description) {
+    protected AbstractBuildDescription(String regex, String description, String regularExpressionForFailed, String descriptionForFailed) {
         this.regex = regex
         this.description = description
+        this.regularExpressionForFailed = regularExpressionForFailed
+        this.descriptionForFailed = descriptionForFailed
     }
 
     Closure toDsl() {
         return {
-            buildDescription(regex, description)
+            buildDescription(regex, description, regularExpressionForFailed, descriptionForFailed)
         }
     }
 }
